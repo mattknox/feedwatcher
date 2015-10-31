d69157a89d747d9727844d8c6561da6c5d65f8dd
mattknox <matthewknox@gmail.com>
2015-10-31 01:08:47 -0700

    working!

--- feedwatcher.rb
+++ feedwatcher.rb
@@ -15,24 +15,35 @@ BEGIN
 
 my_gh = {
   url: "https://api.github.com/users/mattknox/events",
-  time-selector: ["created_at"],
-  filters: [{op: "=", val: "PushEvent", selector: ["type"]},
-            {op: ".match", val: "mattknox/", selector: ["repo", "name"]},
+  time_selector: ["created_at"],
+  filters: [{op: "==", val: "PushEvent", selector: ["type"]},
+            {op: "match", val: "mattknox/", selector: ["repo", "name"]},
            ],
-  data-selector: ["payload", "commits"],
+  data_selector: ["payload", "commits"],
 }
 
 require "httparty"
-require "active_support/time"
+require "active_support/all"
 
 def interpret(spec)
-  response = HTTParty.get spec["url"]
-  relevant = response.select { |elt| Time.parse(elt.send( spec[:time_selector])).today? }
+  response = HTTParty.get spec[:url]
+  relevant = response.select { |elt| relevant_element?(elt, spec) }
+
+end
+
+def relevant_element?(elt, spec)
+  Time.parse(eval_selector(elt, spec[:time_selector])).in_time_zone("Pacific Time (US & Canada)").today? &&
+    spec[:filters].all? {|f| run_filter(elt, f)}
 end
 
 def eval_selector(obj, arr)
   # this evaluates a chain of argumentless selectors
   arr.inject(obj) {|m, x| evapp(m, x) }
+end
+
+def run_filter(obj, f)
+  x = eval_selector(obj, f[:selector])
+  x.send f[:op], f[:val]
 end
 
 def evapp(x, str)
