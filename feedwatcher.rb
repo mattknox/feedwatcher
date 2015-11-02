98430ed494e8b37ad2db3a013a5fca2a519f9f72
mattknox <matthewknox@gmail.com>
2015-11-02 07:34:22 -0800

    wrapped up in a class

--- feedwatcher.rb
+++ feedwatcher.rb
@@ -25,31 +25,43 @@ my_gh = {
 require "httparty"
 require "active_support/all"
 
-def interpret(spec)
-  response = HTTParty.get spec[:url]
-  relevant = response.select { |elt| relevant_element?(elt, spec) }
+module Feedwatcher
+  class Handler
+    ATTRS = [:url, :time_selector, :filters, :data_selector]
+    attr_accessor *ATTRS
 
-end
+    def initialize(h)
+      ATTRS.each do |sym|
+        self.send("#{sym}=", h[sym])
+      end
+    end
 
-def relevant_element?(elt, spec)
-  Time.parse(eval_selector(elt, spec[:time_selector])).in_time_zone("Pacific Time (US & Canada)").today? &&
-    spec[:filters].all? {|f| run_filter(elt, f)}
-end
+    def interpret
+      response = HTTParty.get url
+      relevant = response.select { |elt| relevant_element?(elt) }
+    end
 
-def eval_selector(obj, arr)
-  # this evaluates a chain of argumentless selectors
-  arr.inject(obj) {|m, x| evapp(m, x) }
-end
+    def relevant_element?(elt)
+      Time.parse(eval_selector(elt, time_selector)).in_time_zone("Pacific Time (US & Canada)").today? &&
+        filters.all? {|f| run_filter(elt, f)}
+    end
 
-def run_filter(obj, f)
-  x = eval_selector(obj, f[:selector])
-  x.send f[:op], f[:val]
-end
+    def run_filter(obj, f)
+      x = eval_selector(obj, f[:selector])
+      x.send f[:op], f[:val]
+    end
 
-def evapp(x, str)
-  if "." == str.first
-    x.send(str)
-  else
-    x[str]
+    def eval_selector(obj, arr)
+      # this evaluates a chain of argumentless selectors
+      arr.inject(obj) {|m, x| evapp(m, x) }
+    end
+
+    def evapp(x, str)
+      if "." == str.first
+        x.send(str)
+      else
+        x[str]
+      end
+    end
   end
 end
